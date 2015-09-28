2fabf6b0591d63f8ea2062f2bde4ca2b0a2b2f5e
Christophe Chapuis <chris.chapuis@gmail.com>
2015-09-28 20:19:49 +0200

    WebEngineNewViewRequest: provide the requested URL as a property
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/webengine/api/qquickwebenginenewviewrequest_p.h
+++ src/webengine/api/qquickwebenginenewviewrequest_p.h
@@ -68,6 +68,7 @@ class Q_WEBENGINE_PRIVATE_EXPORT QQuickWebEngineNewViewRequest : public QObject 
     Q_PROPERTY(QUrl requestedUrl READ requestedUrl CONSTANT REVISION 1 FINAL)
     Q_PROPERTY(bool userInitiated READ isUserInitiated CONSTANT FINAL)
     Q_PROPERTY(QStringList additionalFeatures READ additionalFeatures CONSTANT FINAL)
+    Q_PROPERTY(QUrl url READ url)
 public:
     ~QQuickWebEngineNewViewRequest();
 
@@ -76,6 +77,7 @@ public:
     bool isUserInitiated() const;
     const QStringList &additionalFeatures() const;
     Q_INVOKABLE void openIn(QQuickWebEngineView *view);
+    QUrl url() const;
 
 private:
     QQuickWebEngineNewViewRequest();
