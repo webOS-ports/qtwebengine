dfacbc5a948dc3df55224e5850721d4eca091e8d
Christophe Chapuis <chris.chapuis@gmail.com>
2015-09-28 20:19:14 +0200

    WebEngineView: provide additionnal features from window.open on the Qt side
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/webenginewidgets/api/qwebenginepage_p.h
+++ src/webenginewidgets/api/qwebenginepage_p.h
@@ -93,7 +93,7 @@ public:
     virtual void loadFinished(bool success, const QUrl &url, bool isErrorPage = false, int errorCode = 0, const QString &errorDescription = QString()) Q_DECL_OVERRIDE;
     virtual void focusContainer() Q_DECL_OVERRIDE;
     virtual void unhandledKeyEvent(QKeyEvent *event) Q_DECL_OVERRIDE;
-    virtual void adoptNewWindow(QtWebEngineCore::WebContentsAdapter *newWebContents, WindowOpenDisposition disposition, bool userGesture, const QRect &initialGeometry) Q_DECL_OVERRIDE;
+    virtual void adoptNewWindow(QtWebEngineCore::WebContentsAdapter *newWebContents, WindowOpenDisposition disposition, bool userGesture, const QRect &initialGeometry, const QStringList &additionalFeatures) Q_DECL_OVERRIDE;
     virtual bool isBeingAdopted() Q_DECL_OVERRIDE;
     virtual void close() Q_DECL_OVERRIDE;
     virtual void windowCloseRejected() Q_DECL_OVERRIDE;
