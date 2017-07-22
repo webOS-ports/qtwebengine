dd75653567596ced25c50b1b8f86eaae512bffdb
Christophe Chapuis <chris.chapuis@gmail.com>
2017-07-22 09:09:31 +0000

    WebEngineView: re-introduce devicePixelRatio property
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/webengine/api/qquickwebengineview_p.h
+++ src/webengine/api/qquickwebengineview_p.h
@@ -127,6 +127,7 @@ class Q_WEBENGINE_PRIVATE_EXPORT QQuickWebEngineView : public QQuickItem {
     Q_PROPERTY(bool audioMuted READ isAudioMuted WRITE setAudioMuted NOTIFY audioMutedChanged FINAL REVISION 3)
     Q_PROPERTY(bool recentlyAudible READ recentlyAudible NOTIFY recentlyAudibleChanged FINAL REVISION 3)
     Q_PROPERTY(uint webChannelWorld READ webChannelWorld WRITE setWebChannelWorld NOTIFY webChannelWorldChanged REVISION 3 FINAL)
+    Q_PROPERTY(qreal devicePixelRatio READ devicePixelRatio WRITE setDevicePixelRatio NOTIFY devicePixelRatioChanged REVISION 4)
 
     Q_PROPERTY(QQuickWebEngineView *inspectedView READ inspectedView WRITE setInspectedView NOTIFY inspectedViewChanged REVISION 7 FINAL)
     Q_PROPERTY(QQuickWebEngineView *devToolsView READ devToolsView WRITE setDevToolsView NOTIFY devToolsViewChanged REVISION 7 FINAL)
@@ -473,6 +474,8 @@ public:
     bool isAudioMuted() const;
     void setAudioMuted(bool muted);
     bool recentlyAudible() const;
+    qreal devicePixelRatio() const;
+    void setDevicePixelRatio(qreal);
 
 #ifdef ENABLE_QML_TESTSUPPORT_API
     QQuickWebEngineTestSupport *testSupport() const;
@@ -540,6 +543,7 @@ Q_SIGNALS:
     Q_REVISION(4) void colorDialogRequested(QQuickWebEngineColorDialogRequest *request);
     Q_REVISION(4) void fileDialogRequested(QQuickWebEngineFileDialogRequest *request);
     Q_REVISION(4) void formValidationMessageRequested(QQuickWebEngineFormValidationMessageRequest *request);
+    Q_REVISION(4) void devicePixelRatioChanged();
     Q_REVISION(5) void pdfPrintingFinished(const QString &filePath, bool success);
     Q_REVISION(7) void quotaRequested(const QWebEngineQuotaRequest &request);
     Q_REVISION(7) void geometryChangeRequested(const QRect &geometry, const QRect &frameGeometry);
