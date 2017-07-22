1c7abe6ad261743ee9f23ff573b33ffc0029daaf
Christophe Chapuis <chris.chapuis@gmail.com>
2017-07-22 09:41:45 +0000

    WebEngineView: add extraContextMenuEntriesComponent property
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/webengine/api/qquickwebengineview.cpp
+++ src/webengine/api/qquickwebengineview.cpp
@@ -1114,6 +1114,22 @@ void QQuickWebEngineView::setDevicePixelRatio(qreal devicePixelRatio)
     Q_EMIT devicePixelRatioChanged();
 }
 
+void QQuickWebEngineView::setExtraContextMenuEntriesComponent(QQmlComponent *contextMenuExtras)
+{
+    Q_D(QQuickWebEngineView);
+    if (d->contextMenuExtraItems == contextMenuExtras)
+        return;
+    d->contextMenuExtraItems = contextMenuExtras;
+    Q_EMIT extraContextMenuEntriesComponentChanged();
+}
+
+QQmlComponent *QQuickWebEngineView::extraContextMenuEntriesComponent() const
+{
+    Q_D(const QQuickWebEngineView);
+    return d->contextMenuExtraItems;
+}
+
+
 #if QT_CONFIG(webengine_testsupport)
 QQuickWebEngineTestSupport *QQuickWebEngineView::testSupport() const
 {
